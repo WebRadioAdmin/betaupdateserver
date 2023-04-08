0cba36a513f6d539b5e2d902cd1651f04c56c610
WebRadioAdmin <121095041+WebRadioAdmin@users.noreply.github.com>
2023-04-08 14:38:36 +0200

    Update check_product_key.php

--- check_product_key.php
+++ check_product_key.php
@@ -1,22 +1,14 @@
 <?php
-
-// Abrufen des Produktschlüssels aus der GET-Anfrage
-$productKey = $_GET["product_key"];
-
-// Überprüfen der Gültigkeit des Produktschlüssels
-if (checkProductKeyValidity($productKey)) {
-    echo "valid";
+if (isset($_POST['key'])) {
+    $key = $_POST['key'];
+    // Hier sollte der Code zur Überprüfung des Produktschlüssels stehen
+    // Wenn der Schlüssel gültig ist, geben Sie "Valid" zurück, ansonsten "Invalid"
+    if ($key == "123456789") {
+        echo "Valid";
+    } else {
+        echo "Invalid";
+    }
 } else {
-    echo "invalid";
+    echo "Missing key parameter";
 }
-
-// Beispiel-Funktion zur Überprüfung der Gültigkeit des Produktschlüssels
-function checkProductKeyValidity($productKey) {
-    // Hier könnten Sie Ihre eigene Logik zur Überprüfung des Produktschlüssels einfügen,
-    // z.B. durch Abfrage einer Datenbank oder durch Verwendung eines Algorithmus zur Validierung
-    // des Schlüssels. In diesem Beispiel wird der Schlüssel "ABCDE-12345-FGHIJ-67890" als gültig
-    // betrachtet, alle anderen Schlüssel als ungültig.
-    return ($productKey == "ABCDE-12345-FGHIJ-67890");
-}
-
 ?>
